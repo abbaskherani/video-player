a508edbc6b5519790b7692920ef7445144e5d384
abbaskherani <abbaskherani1998@gmail.com>
2024-09-26 17:37:37 +0530

    connect to mongo atlas

---
+++ src/db/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+//db is on another continent
+
+const connectDB = async ()=> {
+try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) //gives a return object
+    console.log(`\n mongodb connected !! db host: ${connectionInstance.connection.host}`);
+} catch (error) {
+    console.log("MONGODB connection failed", error);
+    process.exit(1)
+}
+
+}
+
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,41 @@
+// require('dotenv').config({path: './env'}) //old method
+
+import dotenv from 'dotenv' //new way
+import mongoose from 'mongoose'
+import { DB_NAME } from './constants.js';
+import connectDB from './db/db.js';
+
+dotenv.config({ // new of dot env, add experimental in package.json -r dotenv/config --experimental-json-modules
+    path : './env'
+})
+
+connectDB()
+
+
+
+
+
+/* 1st approach by creating IIFE
+//better approach is the 2nd one professional
+import express from 'express';
+const app = express();
+;(async ()=>{ 
+    //; just for cleaning purpose considering last line me kisi ne ; nhi lagaya hoga
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        app.on("error", (error)=> {
+            console.log("err:", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`app is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("Error", error)
+        throw err
+    }
+})()
+
+*/
